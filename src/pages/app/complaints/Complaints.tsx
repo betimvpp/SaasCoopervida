eec3b6e3cafb99d9b7eba9067cb8d1c662a104a9
Roberto Junior <betimvpp@gmail.com>
2024-11-20 15:32:29 -0300

    build alterarions

--- src/pages/app/complaints/Complaints.tsx
+++ src/pages/app/complaints/Complaints.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Complaints = () => {
   return (
     <div>Complaints</div>
--- src/pages/app/payments/Payments.tsx
+++ src/pages/app/payments/Payments.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Payments = () => {
   return (
     <div>Payments</div>
